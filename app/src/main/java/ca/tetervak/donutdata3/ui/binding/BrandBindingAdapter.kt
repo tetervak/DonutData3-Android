452fb7cf5bfa5113026b4dcc5e8d9dc4d4c11e22
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-10-11 11:13:48 -0400

    displayed the brand name in the list

---
+++ app/src/main/java/ca/tetervak/donutdata3/ui/binding/BrandBindingAdapter.kt
@@ -0,0 +1,14 @@
+package ca.tetervak.donutdata3.ui.binding
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import ca.tetervak.donutdata3.R
+import ca.tetervak.donutdata3.domain.Brand
+
+@BindingAdapter("brandName")
+fun bindBrandName(textView: TextView, brand: Brand?){
+    if(brand is Brand){
+       val brandNames = textView.resources.getStringArray(R.array.donut_brand_name)
+       textView.text = brandNames[brand.ordinal]
+    }
+}
